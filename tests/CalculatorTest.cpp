2080173357f6e1fbb8d21e576337fd82d3de5956
silva-nick <silva.d.nicholas@gmail.com>
2021-12-23 12:47:58 -0600

    set up gtest

---
+++ tests/CalculatorTest.cpp
@@ -0,0 +1,13 @@
+#include "../include/calc.hpp"
+
+#include "gtest/gtest.h"
+
+namespace {
+
+using namespace bluefin;
+
+TEST(Calculator, AdditionTest) {
+    Interpreter intr("1+1");
+    EXPECT_EQ(intr.parse(), 2); 
+}
+} // namespace
